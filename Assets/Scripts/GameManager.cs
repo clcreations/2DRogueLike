2a4454ccc27f977bcefd285d6aa1c048cd0fb0ba
clcreations <clcreations@users.noreply.github.com>
2016-01-22 19:07:25 -0600

    Initial

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public float levelStartDelay = 2f;
+    public float turnDelay = 0.1f;
+    public static GameManager instance = null;
+    public BoardManager boardScript;
+    public int playerFoodPoints = 100;
+    [HideInInspector] public bool playersTurn = true;
+
+    int level = 1;
+    List<Enemy> enemies;
+    bool enemiesMoving;
+    Text levelText;
+    GameObject levelImage;
+    bool doingSetup;
+
+    void Awake(){
+        if (instance == null){
+            instance = this;
+        }else{
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+        enemies = new List<Enemy>();
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+    }
+
+    void OnLevelWasLoaded(int index){
+        // Complete hack because Unity 5.3.1 has a bug, and this fixes it
+        // (bug only appears on build, runs fine in editor)
+        if (Time.time > 6f) {
+
+            level++;
+
+            InitGame();
+        }
+    }
+
+    void InitGame(){
+        doingSetup = true;
+
+        levelImage = GameObject.Find("LevelImage");
+        levelText = GameObject.Find("LevelText").GetComponent<Text>();
+
+        levelText.text = "Day " + level;
+        levelImage.SetActive(true);
+        Invoke("HideLevelImage", levelStartDelay);
+        enemies.Clear();
+        boardScript.SetupScene(level);
+    }
+
+    private void HideLevelImage(){
+        levelImage.SetActive(false);
+        doingSetup = false;
+    }
+
+    public void GameOver(){
+        levelText.text = "After " + level + " days, you starved.";
+        levelImage.SetActive(true);
+        enabled = false;
+    }
+
+    void Update(){
+        if (playersTurn || enemiesMoving || doingSetup){
+            return;
+        }
+
+        StartCoroutine(MoveEnemies());
+    }
+
+    public void AddEnemyToList(Enemy script){
+        enemies.Add(script);
+    }
+
+    IEnumerator MoveEnemies(){
+        enemiesMoving = true;
+        yield return new WaitForSeconds(turnDelay);
+        if (enemies.Count == 0){
+            yield return new WaitForSeconds(turnDelay);
+        }
+
+        //for (int i = 0; i < enemies.Count; i++){
+        //}
+        foreach(Enemy enemy in enemies){
+            enemy.MoveEnemy();
+            yield return new WaitForSeconds(enemy.moveTime);
+        }
+
+        playersTurn = true;
+        enemiesMoving = false;
+    }
+
+} // public class GameManager : MonoBehaviour {
